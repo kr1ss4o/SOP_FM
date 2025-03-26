b02412a8be35429f3b55a1c75820813a27c6d339
Kristiyan Tsenov <kristiyantsenov2007@gmail.com>
2025-03-26 11:34:19 +0200

    Beta release 1.0 (minecraft type shit)
    Features:
    - Top artists page;
    - Navigation bar feature;
    - Fixed page redirections;

--- SpotifyTracker/obj/Debug/net8.0/SpotifyTracker.AssemblyInfo.cs
+++ SpotifyTracker/obj/Debug/net8.0/SpotifyTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpotifyTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df256d05dc8b1483ddb9749695abc780a9a5ab6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpotifyTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpotifyTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
