d9a703e1625550f6f11752b9102e0be9b10b8554
Kristiyan Tsenov <kristiyantsenov2007@gmail.com>
2025-06-18 22:35:50 +0300

    Initial Release 1.0
    Added the footer. The website is ready to go.

--- SpotifyTracker/obj/Debug/net6.0/SpotifyTracker.AssemblyInfo.cs
+++ SpotifyTracker/obj/Debug/net6.0/SpotifyTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpotifyTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30decda9ff4cff76e646d0bfc32d4eb8f123af72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab3f390bf8a29f36e12a9bf34eb1c5a7eb2fd7ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpotifyTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpotifyTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
